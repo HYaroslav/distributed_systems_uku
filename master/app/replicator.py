c0654177800c8309996161d19e8a7ea3d6c9e7ef
HYaroslav <gslaavko@gmail.com>
2022-11-01 17:09:00 +0200

    Task 1: reworked thread managing.

--- master/app/replicator.py
+++ master/app/replicator.py
@@ -14,22 +14,17 @@ class Replicator():
 
     def __init__(self, start_port: int, secondaries_number: int) -> None:
         self.logger = logging.getLogger(__name__)
-
-        self.logger.info("hello")
         
         for i in range(secondaries_number):
             # IPv4 for secondaries will start from 172.20.0.3
             endpoint = SECONDARY_LISTENER_ADDRESS.format(host_address=i+3, port=start_port+i+1)
             self.secondary_endpoints_list.append(endpoint)
 
-        self.replication_queue = queue.Queue()
-        threading.Thread(target=self._queue_worker, daemon=True).start()
 
     def replicate(self, data: str):
         secondaries_number = len(self.secondary_endpoints_list)
-        thread_barrier = threading.Barrier(secondaries_number+2, timeout=20)
+        thread_barrier = threading.Barrier(secondaries_number+1, timeout=20)
 
-        thread_list: List[threading.Thread] = []
         for replica_url in self.secondary_endpoints_list:
             thread = threading.Thread(
                 target=self._replicate,
@@ -39,9 +34,7 @@ class Replicator():
                     "thread_barrier": thread_barrier
                 }
             )
-            thread_list.append(thread)
-
-        self.replication_queue.put([thread_list, thread_barrier])
+            thread.start()
 
         try:
             thread_barrier.wait()
@@ -51,20 +44,6 @@ class Replicator():
         
         self.logger.info("Replication of msg='%s' to %s nodes was done successfuly!", data, secondaries_number)
 
-
-    def _queue_worker(self):
-        while True:
-            queue_item = self.replication_queue.get()
-            thread_list: List[threading.Thread] = queue_item[0]
-            thread_barrier: threading.Barrier = queue_item[1]
-
-            for replica_thread in thread_list:
-                replica_thread.start()
-            
-            thread_barrier.wait()
-
-            self.replication_queue.task_done()
-            self.logger.info("Queue - task done.")
     
     def _replicate(self, data: str, endpoint_url: str, thread_barrier: threading.Barrier):
         host, port = endpoint_url.split(":")

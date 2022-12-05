ff0b0c1c9c350a2a51f8814582ddc7535b897f2b
HYaroslav <gslaavko@gmail.com>
2022-12-05 18:45:10 +0200

    Task 2. Added new features - async replication, message ordering, message postpones, write concerns.

--- master/app/replicator.py
+++ master/app/replicator.py
@@ -1,29 +1,80 @@
+import json
 import logging
 import threading
 from typing import List
 import socket
-import queue
+
+
+class ExtendedBarrier(threading.Barrier):
+    """Class for supporting extra Threads over the Barrier parties.
+
+    `_state` variable can also have value 4 now - released state
+    """
+
+    def wait(self, timeout=None):
+        """Wait for the barrier.
+
+        When the specified number of threads have started waiting, they are all
+        simultaneously awoken. If an 'action' was provided for the barrier, one
+        of the threads will have executed that callback prior to returning.
+        Returns an individual index number from 0 to 'parties-1'.
+
+        """
+        # if Barrier has already been released then return
+        if self._state == 4:
+            return self._count
+
+        if timeout is None:
+            timeout = self._timeout
+        with self._cond:
+            self._enter() # Block while the barrier drains.
+            index = self._count
+            self._count += 1
+            try:
+                if index + 1 == self._parties:
+                    # We release the barrier
+                    self._release()
+                else:
+                    # We wait until someone releases us
+                    self._wait(timeout)
+                return index
+            finally:
+                self._count -= 1
+                # Wake up any threads waiting for barrier to drain.
+                self._exit()
+
+    def _exit(self):
+        if self._count == 0:
+            if self._state in (-1, 1):
+                # Mark barrier as released
+                self._state = 4
+                self._cond.notify_all()    
 
 
 class Replicator():
 
     secondary_endpoints_list: List[str] = []
     logger: logging.Logger
-    replication_queue: queue.Queue
+    message_id: int
 
     def __init__(self, secondary_endpoints_list) -> None:
         self.logger = logging.getLogger(__name__)
         self.secondary_endpoints_list = secondary_endpoints_list
 
+        self.message_id = 1
 
-    def replicate(self, data: str):
-        secondaries_number = len(self.secondary_endpoints_list)
-        thread_barrier = threading.Barrier(secondaries_number+1, timeout=20)
+
+    def replicate(self, data: str, write_concern: int):
+        message_id = self.message_id
+        self.message_id += 1
+
+        thread_barrier = ExtendedBarrier(write_concern, timeout=20)
 
         for replica_url in self.secondary_endpoints_list:
             thread = threading.Thread(
                 target=self._replicate,
                 kwargs={
+                    "message_id": message_id,
                     "data": data,
                     "endpoint_url": replica_url,
                     "thread_barrier": thread_barrier
@@ -37,17 +88,29 @@ class Replicator():
             self.logger.error("Some nodes take too long time to process.")
             return
         
-        self.logger.info("Replication of msg='%s' to %s nodes was done successfuly!", data, secondaries_number)
+        self.logger.info("Replication of msg='%s' to 1 master and %s nodes was done successfuly!", data, write_concern-1)
 
     
-    def _replicate(self, data: str, endpoint_url: str, thread_barrier: threading.Barrier):
+    def _replicate(self, message_id: int, data: str, endpoint_url: str, thread_barrier: ExtendedBarrier):
         host, port = endpoint_url.split(":")
+
+        message = json.dumps({
+            "message_id": message_id,
+            "message": data
+        })
+        
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, int(port)))
-            s.sendall(bytes(data, encoding="utf8"))
+            s.sendall(message.encode(encoding="utf-8"))
 
             response_data = s.recv(1024).decode("utf-8")
             self.logger.info(response_data)
+
+            if response_data.startswith("Error"):
+                thread_barrier.abort()
+                s.close()
+                return
+
             s.close()
 
         thread_barrier.wait()
--- secondary/app/replications_listener.py
+++ secondary/app/replications_listener.py
@@ -1,7 +1,8 @@
+import json
 import socket
 import threading
 import logging
-from typing import List
+from typing import List, Tuple
 from time import sleep
 import random
 
@@ -10,37 +11,86 @@ class ReplicationListener():
     server_socket: socket.socket
     logger: logging.Logger
 
+    _postponed_messages_list: List[Tuple[int, str]]
+
     def __init__(self, ip: str, port: str) -> None:
         self.ip = ip
         self.port = port
         self.listener_port = int(port) + 1
         self.logger = logging.getLogger(__name__)
+        self._postponed_messages_list = []
 
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 
-    def listen(self, db_list: List[str]):
-        listen_thread = threading.Thread(target=self._listen, kwargs={"db_list":db_list})
+    def listen(self, db_list: List[Tuple[int, str]]):
+        listen_thread = threading.Thread(target=self._listen, kwargs={"db_list": db_list})
         listen_thread.start()
+
+
+    def _process_new_message(self, message_id: int, message: str, db_list: List[Tuple[int, str]]):
+        def _is_message_should_be_next():
+            if not db_list and message_id != 1:
+                return False
+            else:
+                return message_id == 1 or max(db_list, key=lambda x: x[0])[0] + 1 == message_id
+
+        msg_tuple = (message_id, message)
+        
+        if _is_message_should_be_next():
+            db_list.append(msg_tuple)
+            if self._postponed_messages_list:
+                post_message_id, post_message = self._postponed_messages_list.pop(0)
+                self._process_new_message(post_message_id, post_message, db_list)
+            return
+
+        if msg_tuple in self._postponed_messages_list:
+            return
+
+        self._postponed_messages_list.append(msg_tuple)
+        self._postponed_messages_list = sorted(self._postponed_messages_list, key=lambda x: x[0])
+        
         
 
-    def _listen(self, db_list: List[str]):
+    def _listen(self, db_list: List[Tuple[int, str]]):
+        def listen_on_new_client(client_socket: socket.socket, client_addr):
+            self.logger.info("Accepted a replication request from %s:%s"%(client_addr[0], client_addr[1]))
+            while True:
+                dataFromClient = json.loads(
+                    client_socket.recv(1024).decode("utf-8")
+                )
+
+                message_id = dataFromClient["message_id"]
+                message = dataFromClient["message"]
+
+                # imitate delays
+                rand_int = random.randint(2, 10)
+                self.logger.info("Data will be replicated in %s seconds.", rand_int)
+                sleep(rand_int)
+
+                try:
+                    self._process_new_message(message_id, message, db_list)
+                    self.logger.info("Data is successfully replicated.")
+                    response = f"Message is successfully replicated on {self.ip}:{self.port}".encode()
+                except Exception as e:
+                    self.logger.error("An error occured in data replication.")
+                    response = f"Error on {self.ip}:{self.port} - {e}".encode()
+                finally:
+                    client_socket.send(response)
+                break
+
+            client_socket.close()
+
+
         self.server_socket.bind((self.ip, self.listener_port))
         self.server_socket.listen()
         self.logger.info(f"Replication listener started on {self.ip}:{self.listener_port}")
 
-        while(True):
-            (clientConnected, clientAddress) = self.server_socket.accept()
-            self.logger.info("Accepted a replication request from %s:%s"%(clientAddress[0], clientAddress[1]))
+        while True:
+            (client_connected, client_address) = self.server_socket.accept()
 
-            dataFromClient = clientConnected.recv(1024).decode("utf-8")
+            new_client_thread = threading.Thread(target=listen_on_new_client, args=(client_connected, client_address))
+            new_client_thread.start()
 
-            # imitate lagging
-            rand_int = random.randint(2, 5)
-            self.logger.info("Data will be replicated in %s seconds.", rand_int)
-            sleep(rand_int)
             
-            db_list.append(dataFromClient)
-            self.logger.info("Data is successfully replicated.")
-            clientConnected.send(f"Message is successfully replicated on {self.ip}:{self.port}".encode())
